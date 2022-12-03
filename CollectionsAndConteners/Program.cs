bcaa423278b9260b82304b1d907fccc25f1ca0e6
P8ON-code <papasteryd8@gmail.com>
2022-12-03 16:19:35 +0100

    Dodaj pliki projektów.

---
+++ CollectionsAndConteners/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CollectionsAndConteners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            Console.WriteLine(list.Count);
+            int myInt = 20;
+            list.Add(myInt);
+            Console.WriteLine(list.Count);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list.ElementAt<int>(0));
+            try
+            {
+
+                Console.WriteLine(list.ElementAt<int>(1)); //Exception - Out of Range
+                Console.WriteLine(list[0]); //Exception - Out of Range
+
+
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+    }
+}
